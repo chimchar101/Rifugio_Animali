5066afd69bad4830f2b70570f5f9e9396d37dec5
Andrearttr <andrearottura03@gmail.com>
2025-06-20 17:14:13 +0200

    aggiunta query main

--- RifugioAnimali/Main.cs
+++ RifugioAnimali/Main.cs
@@ -7,3 +7,13 @@
 FROM 
     (SELECT COUNT(*) AS TotaleAdozioni FROM adozione) AS A,
     (SELECT COUNT(*) AS TotaleAnimali FROM animale) AS B; */
+
+/* -- Quanto tempo resta mediamente un animale prima di essere adottato?
+select sum(to_days(ad.data) - to_days(ing.data)) / count(distinct ad.adozione_id) as permanenza_media
+from adozione ad
+join animale an on an.animale_id = ad.animale_id
+join ingresso ing on ing.animale_id = an.animale_id
+where an.animale_id in(
+	select animale_id from adozione
+)
+; */
